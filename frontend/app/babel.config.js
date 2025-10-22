7cf70c5a70c28ce95d17eedb954d557067eda41e
europanite <europanite@gmail.com>
2025-10-22 14:11:17 +0900

    initial commit

--- frontend/app/babel.config.js
+++ frontend/app/babel.config.js
@@ -2,14 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    env: {
-      test: {
-        plugins: [
-          ['transform-inline-environment-variables', {
-            include: ['EXPO_PUBLIC_API_BASE']
-          }],
-        ],
-      },
-    },
   };
 };
