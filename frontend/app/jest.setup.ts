845b61835d1ea3e1de477da2866c6d2616e4d4a1
europanite <europanite@gmail.com>
2025-10-22 15:17:48 +0900

    initial commit

--- frontend/app/jest.setup.ts
+++ frontend/app/jest.setup.ts
@@ -1,7 +1,2 @@
-// Make matchers optional so missing packages or environment differences don't crash CI.
-try {
-  require('@testing-library/jest-native/extend-expect');
-} catch {}
-try {
-  require('@testing-library/jest-dom');
-} catch {}
+import '@testing-library/jest-native/extend-expect';
+import '@testing-library/jest-dom';
